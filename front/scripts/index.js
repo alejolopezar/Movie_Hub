248a014d9c0477e4fbc0162bcd6ff8bbe5982a20
alejolopezar <alejo131996@hotmail.com>
2024-09-13 20:38:11 -0500

    M2|Clase 6 homework

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,22 +1,44 @@
 //se importa el modulo de renderCards y tempData
 const { contenedor_tarjetas, crearTarjeta } = require("./renderCards");
+const axios = require("axios");
 const { tempData } = require("./tempData");
 
-// Se hace la solicitud GET. Esta solicitud es asincrona
-$.get("https://students-api.up.railway.app/movies")
-.done((data) => {
-    //"Mapear" el listado de peliculas para convertirlos en elementos HTML
-    const arrayTarjetas = data.map(crearTarjeta);  
+// // Se hace la solicitud GET. Esta solicitud es asincrona
+// $.get("https://students-api.up.railway.app/movies")
+// .done((data) => {
+//     //"Mapear" el listado de peliculas para convertirlos en elementos HTML
+//     const arrayTarjetas = data.map(crearTarjeta);  
     
-    //"Appendear" todos los elementos HTML del nuevo array dentro del contenedor
-    arrayTarjetas.forEach(tarjeta => contenedor_tarjetas.appendChild(tarjeta));
-})
+//     //"Appendear" todos los elementos HTML del nuevo array dentro del contenedor
+//     arrayTarjetas.forEach(tarjeta => contenedor_tarjetas.appendChild(tarjeta));
+// })
 
-.fail(() => {
-    alert("La información del servidor no se cargo, se utilizara información de tempData");
-    //"Mapear" el listado de peliculas para convertirlos en elementos HTML
-    const arrayTarjetas = tempData.map(crearTarjeta);
+// .fail(() => {
+//     alert("La información del servidor no se cargo, se utilizara información de tempData");
+//     //"Mapear" el listado de peliculas para convertirlos en elementos HTML
+//     const arrayTarjetas = tempData.map(crearTarjeta);
     
-    //"Appendear" todos los elementos HTML del nuevo array dentro del contenedor
-    arrayTarjetas.forEach(tarjeta => contenedor_tarjetas.appendChild(tarjeta));
-});
+//     //"Appendear" todos los elementos HTML del nuevo array dentro del contenedor
+//     arrayTarjetas.forEach(tarjeta => contenedor_tarjetas.appendChild(tarjeta));
+// });
+
+const promise = axios.get("https://students-api.up.railway.app/movies");
+console.log(promise);
+promise
+.then((res) => {
+    //"Mapear" el listado de peliculas para convertirlos en elementos HTML
+    const arrayTarjetas = res.data.map(crearTarjeta);  
+    
+     //"Appendear" todos los elementos HTML del nuevo array dentro del contenedor
+     arrayTarjetas.forEach(tarjeta => contenedor_tarjetas.appendChild(tarjeta));
+     console.log("se ejecuto then");
+})
+.catch(() => {
+    alert("La información del servidor no se cargo, se utilizara información de tempData");
+     //"Mapear" el listado de peliculas para convertirlos en elementos HTML
+     const arrayTarjetas = tempData.map(crearTarjeta);
+    
+     //"Appendear" todos los elementos HTML del nuevo array dentro del contenedor
+     arrayTarjetas.forEach(tarjeta => contenedor_tarjetas.appendChild(tarjeta));
+     console.log("se ejecuto catch");
+})
